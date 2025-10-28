a781cc794ac998d44e70e6d7c0943d8eec86d1c0
Tt <tangtao@ascs.tech>
2025-10-28 16:19:30 +0800

    fix: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,7 @@ import { TaskList } from "@/features/task/components/task-list";
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
-      <Heading
-        title="All Tasks"
-        description="Tasks by everyone at one place 1"
-      />
+      <Heading title="All Tasks" description="Tasks by everyone at one place" />
 
       <ErrorBoundary fallback={<Placeholder label="Something went wrong!" />}>
         <Suspense fallback={<Spinner />}>
